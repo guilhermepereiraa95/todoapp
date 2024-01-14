61bd0814aede9584ff449a8f0969aa004ca08335
guilhermepereira <guilhermepereira@apoioprodesp.sp.gov.br>
2024-01-14 12:27:12 -0300

    chore: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projeto';
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+    } else {
+      alert('Type a something to add a task!')
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
